69ebcd43016bd26396bb73f64bcb61d9549a621c
kwl57 <105624431+kwl57@users.noreply.github.com>
2024-08-29 16:43:52 +0400

    Add files via upload

---
+++ modle_1_9.py
@@ -0,0 +1,10 @@
+my_string = input()
+print(my_string.__len__())
+print(my_string.upper())
+print(my_string.lower())
+
+print(my_string.strip())
+my_string = my_string.strip()
+print(my_string[:1])
+ln = my_string.__len__()
+print(my_string[ln -1:])
