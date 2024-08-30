fd34fff512a682122d2a2c080f8908f77717aec3
kwl57 <105624431+kwl57@users.noreply.github.com>
2024-08-30 08:55:20 +0400

    Update modle_1_9.py

--- modle_1_9.py
+++ modle_1_9.py
@@ -1,10 +1,15 @@
+
 my_string = input()
 print(my_string.__len__())
 print(my_string.upper())
 print(my_string.lower())
 
-print(my_string.strip())
-my_string = my_string.strip()
+
+my_string = my_string.split()
+my_string = ''.join(my_string)
+print(my_string)
+print(my_string.__len__())
+
 print(my_string[:1])
 ln = my_string.__len__()
 print(my_string[ln -1:])
