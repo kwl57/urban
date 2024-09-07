a6c838eeb289a2f5644d6e19bd060ae10d5beaee
kwl57 <105624431+kwl57@users.noreply.github.com>
2024-09-08 01:02:59 +0400

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,15 +1,26 @@
-def send_email(message, recipient, *, sender="university.help@gmail.com"):
-    if recipient == sender:
-        print("Нельзя отправить письмо самому себе!")
-    elif sender == "university.help@gmail.com":
-        print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient)
-    elif ("@" and (".com" or ".ru" or ".net")) not in recipient and ("@" and (".com" or ".ru" or ".net")) not in sender:
-        print("Невозможно отправить письмо с адреса", sender, "на адрес", recipient)
-    else:
-        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
+def send_email(message, recipient, sender = "university.help@gmail.com"):
+    f = recipient.find("@")
+    e1 = recipient.endswith(".com")
+    e2 = recipient.endswith(".ru")
+    e3 = recipient.endswith(".not")
 
+    g = sender.find("@")
+    w1 = sender.endswith(".com")
+    w2 = sender.endswith(".ru")
+    w3 = sender.endswith(".not")
 
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+    if g == 0:
+        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <", sender, "> на адрес <", recipient, ">.")
+
+    if w1 == False and w2 == False and w3 == False:
+        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <",sender,"> на адрес <",recipient,">.")
+
+    elif w1 == True or w2 == True or w3 == True or g > 0:
+        if e1 == True or e2 == True or e3 == True:
+            if f >  0:
+                if recipient == sender:
+                    print("Нельзя отправить письмо самому себе!")
+                else:
+                    print("Письмо успешно отправлено с адреса <", sender, "> на адрес <", recipient, ">.")
+            else:
+                print( f"Невозможно отправить письмо с адреса ,{sender}, на адрес {recipient}")
