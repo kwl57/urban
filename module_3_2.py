d5293f27cd69c84bc87bc484e8f62989132662a6
kwl57 <105624431+kwl57@users.noreply.github.com>
2024-09-09 00:18:09 +0400

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -2,30 +2,36 @@ def send_email(message, recipient, sender = "university.help@gmail.com"):
     f = recipient.find("@")
     e1 = recipient.endswith(".com")
     e2 = recipient.endswith(".ru")
-    e3 = recipient.endswith(".not")
+    e3 = recipient.endswith(".net")
 
     g = sender.find("@")
     w1 = sender.endswith(".com")
     w2 = sender.endswith(".ru")
-    w3 = sender.endswith(".not")
+    w3 = sender.endswith(".net")
+    #print(g, w1, w2, w3)
+    #print(f, e1, e2, e3)
 
-    if g == 0:
-        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <", sender, "> на адрес <", recipient, ">.")
+    if g<0:
+        print(f"Ошибка нет @ в {sender}")
+    elif f<0:
+        print(f"Ошибка нет @ в {recipient}")
+    elif w1 == False and w2 == False and w3 == False :
+                print(f"Ошибка1 допустимо (.com .ru .net)  {sender}")
+    elif e1 == False and e2 == False and e3 == False :
+                print(f"Ошибка2 допустимо (.com .ru .net) {recipient}")
 
-    if w1 == False and w2 == False and w3 == False:
-        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <",sender,"> на адрес <",recipient,">.")
 
-    elif w1 == True or w2 == True or w3 == True or g > 0:
-        if e1 == True or e2 == True or e3 == True:
-            if f >  0:
-                if recipient == sender:
-                    print("Нельзя отправить письмо самому себе!")
-                else:
-                    print("Письмо успешно отправлено с адреса <", sender, "> на адрес <", recipient, ">.")
-            else:
-                print( f"Невозможно отправить письмо с адреса ,{sender}, на адрес {recipient}")
+    elif recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+    elif sender != "university.help@gmail.com":
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {recipient} на адрес {sender}.")
 
-send_email('Это сообщение для проверки связи', 'vasyok1337gmail.com')
+
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
