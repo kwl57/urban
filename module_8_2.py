2eb0c2221e1784a314525c8bafe484a088fc963a
kwl57 <105624431+kwl57@users.noreply.github.com>
2024-10-29 12:40:15 +0400

    Update module_8_2.py

--- module_8_2.py
+++ module_8_2.py
@@ -21,6 +21,7 @@ def calculate_average(numbers):
 
     return average
 
+
 print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
