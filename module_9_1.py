3c2c11a840c78d879c511068776038d9fc08f757
kwl57 <105624431+kwl57@users.noreply.github.com>
2024-11-02 11:55:30 +0400

    Update module_9_1.py

--- module_9_1.py
+++ module_9_1.py
@@ -4,7 +4,5 @@ def apply_all_func(int_list, *functions):
         result[str(func.__name__)] = func(int_list)
     return result
 
-
-
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
