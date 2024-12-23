cb6bc4911d6abbf89deeaabeaefc64b30e3fc80a
kwl57 <105624431+kwl57@users.noreply.github.com>
2024-12-24 00:12:26 +0400

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,29 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = "7941202954:AAHtiIGpwD_lGygMuOIXuZiQd_a4zwJRXJ4"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(text=['Я', 'учусь'])
+async def s_message(message):
+    print("message")
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print("Привет! Я бот помогающий твоему здоровью.TEST")
+    await message.answer("Рады вас видеть в нашем боте")
+
+
+@dp.message_handler()
+async def all_messages(message):
+    print("Введите команду /start, чтобы начать общение.TEST2")
+    await message.answer(message.text)
+    print(message.text)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
